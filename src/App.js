e6a8c149bd2c3a2da4e1e98808ee608c52f4c6e4
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-06-27 22:07:57 +0200

    Fix the warnings

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Header from "./components/Header";
 import "./assets/css/main.css";
 import Home from "./components/Home";
-import Work from "./components/Work";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Work from "./components/Work";
+// import About from "./components/About";
+// import Portfolio from "./components/Portfolio";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 
 class App extends React.Component {
   state = {
