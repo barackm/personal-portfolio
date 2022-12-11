3c367052f951d76fb8d5a3b829b20c50cd85c732
Barack Mukelenga <barackmukelenga100@gmail.com>
2022-12-11 15:16:12 +0200

    Connect to hosted API

--- src/utlis/constants.js
+++ src/utlis/constants.js
@@ -1,5 +1,5 @@
 const ROOT = process && process.env.REACT_APP_API_ROOT;
-export const DOMAIN = `http://${ROOT}`;
+export const DOMAIN = `https://${ROOT}`;
 export const API_URL = `${DOMAIN}/api/`;
 export const API_END_POINT = `${API_URL}`;
 export const AUTH_TOKEN = 'authToken';
