cb897a842577be2efd49ea3ceac7b6509dee893c
Baraka mukelenga <baraka.m@code-care.pro>
2024-09-28 16:20:50 +0200

    Update Vite configuration to include base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Alias @ to the src directory
