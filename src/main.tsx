7153c56e6f6a9b0c9dc4d3c1826a29ffed54d700
Baraka mukelenga <baraka.m@code-care.pro>
2024-10-02 13:33:31 +0200

    Update favicon and page title

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  build: {
-    outDir: "dist",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  base: "",
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    open: true,
+    port: 5000,
   },
 });
