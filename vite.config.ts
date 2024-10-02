78712f77313fe22a637480fb4048debe973d2c88
Baraka mukelenga <baraka.m@code-care.pro>
2024-10-02 13:48:47 +0200

    Update dependencies and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
-  base: "",
-  plugins: [react(), viteTsconfigPaths()],
-  server: {
-    open: true,
-    port: 5000,
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
